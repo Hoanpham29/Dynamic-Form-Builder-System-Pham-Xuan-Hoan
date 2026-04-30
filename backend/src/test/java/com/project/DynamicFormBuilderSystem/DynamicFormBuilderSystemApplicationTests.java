f5c0ba3193fa8c4cf0f6728c799f578d6a595187
Hoan Pham <hoanpham2911@gmail.com>
2026-04-30 19:46:57 +0700

    chore: initialize Dynamic Form Builder System project

---
+++ backend/src/test/java/com/project/DynamicFormBuilderSystem/DynamicFormBuilderSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.project.DynamicFormBuilderSystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DynamicFormBuilderSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
